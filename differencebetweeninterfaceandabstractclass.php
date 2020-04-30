48e9638795602b050c92389ea5e3d4c32962ec9a
ishmam ali <ishmamalikhan@gmail.com>
2020-04-30 23:08:07 +0500

    interface and abstract class

---
+++ differencebetweeninterfaceandabstractclass.php
@@ -0,0 +1,11 @@
+<?php
+//difference between interface and abstract class
+
+
+
+//an abstract class can have only abstract methods or only non-abstract methods or both,but all method of an interface are abstract by default
+//an abstract class can declare propeerties/methods with access spcifier, but interface can declare only constant properties(no other type properties) and methods are by default abstract and public
+// a class can inherit only one abstract class and multiple inheritance is not possible for abstract class but A class can implement more than one  interface and can achieve multiple inheritance
+//if a class contains even a singal abstract method that class must be declared as abstract
+// in an abstract class, you can defined as well as it's body methods but in the interface you can define only methods
+?>
