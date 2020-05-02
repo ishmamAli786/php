1509e0b9c66ce3c78da065d33f09e1bdf510195a
ishmam ali <ishmamalikhan@gmail.com>
2020-05-02 16:59:28 +0500

    delete data

--- deletedata.php
+++ deletedata.php
@@ -1,3 +1,12 @@
 <?php
 // delete data by procedural method
+$con=mysqli_connect('localhost','root','','checking') or die ('database not connected');
+$q="delete from info where id=1";
+$result=mysqli_query($con,$q);
+if($result){
+    echo "record deleted successfully";
+}
+else{
+    echo "record failed to delete";
+}
 ?>
