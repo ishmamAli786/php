5ea1d32bf6113c8932de17fdd70a32b4ce8029c1
ishmam ali <ishmamalikhan@gmail.com>
2020-05-03 01:30:59 +0500

    difference and similiary bewtenn mysqli and pdo

---
+++ differencebetweensqliandpdo.php
@@ -0,0 +1,19 @@
+<?php
+// Difference and  similarities between mysqli and pdo
+
+
+
+
+//Differencies
+//mysqli support dual interface procedural and object oriented while pdo support only object oriented
+// mysqli will  work with only mysql database while pdo will work on 12 different databases
+
+
+
+//similarities
+// both support prepared statment
+// both provide same level of  security
+// both support object oriented
+//  
+
+?>
