9f1b34ef1707d654706a8cd106dd528db16683a9
ishmam ali <ishmamalikhan@gmail.com>
2020-05-03 00:35:20 +0500

    introduction of pdo

---
+++ pdo.php
@@ -0,0 +1,36 @@
+<?php
+// there are three main api option when consedring connecting to a mysqli database server
+//php's mysql extension (deprecated)
+// php's mysqli extension
+// php Data Object(PDO)
+
+// pdo is a database access layer which provide a fast and consistent interface
+//for accessing and managing database in PHP applications. Every DBMs has specific
+// pdo deriver(s) that must be installed when you are using pdo in php application
+//it support only object  oriented interface.pdo will work on 12 different database system
+
+
+// benefits of pdo
+// usability
+// error handling
+// multiple database
+// secure
+// reusability
+// exception class
+
+
+
+//supported databases
+// CUBRID
+// MS SQL SERVER Sybase
+// firebird
+// IBM
+// Informix
+// MYSQL
+// MS SQL SERVER
+// ORACLE
+// ODBC and DB2
+// PostgreSQL
+// SQLITE
+// 4D
+?>
