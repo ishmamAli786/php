bd2df70104466210bd02482a43eb30cd5298358b
ishmam ali <ishmamalikhan@gmail.com>
2020-05-12 03:26:01 +0500

    filter

---
+++ filter.php
@@ -0,0 +1,10 @@
+<?php
+// php filters are used to validate and sanitize external inputs.
+// this is specially usefull when the data source contains knwon data like user input
+// for example data from an html form cookies sqli statment result etc.
+
+
+// there are two types of filter
+// validation
+// sanitization inapproprities data removed
+?>
